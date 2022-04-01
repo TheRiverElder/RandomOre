90f333ccaa340341e8ec3a61f73f41811f1fba5a
TheRiverElder <765375399@qq.com>
2022-04-01 21:41:11 +0800

    use random tick block instead

--- src/main/java/io/github/theriverelder/randomore/block/RandomOreBlock.java
+++ src/main/java/io/github/theriverelder/randomore/block/RandomOreBlock.java
@@ -6,9 +6,14 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.OreBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.context.LootContext;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.util.math.BlockPos;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class RandomOreBlock extends OreBlock {
 
@@ -22,13 +27,29 @@ public class RandomOreBlock extends OreBlock {
             Blocks.EMERALD_ORE
     );
 
+    public static final IntProperty ORE_TYPE = IntProperty.of("ore_type", 0, ORES.size() - 1);
+
+    private static final Random random = new Random();
+
     public RandomOreBlock(Settings settings) {
         super(settings);
+        setDefaultState(getStateManager().getDefaultState().with(ORE_TYPE, random.nextInt(ORES.size())));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(ORE_TYPE);
     }
 
     @Override
     public List<ItemStack> getDroppedStacks(BlockState state, LootContext.Builder builder) {
-        Block ore = ORES.get((int) (Math.random() * ORES.size()));
+//        Block ore = ORES.get(random.nextInt(ORES.size()));
+        Block ore = ORES.get(state.get(ORE_TYPE) % ORES.size());
         return ore.getDroppedStacks(ore.getDefaultState(), builder);
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        world.setBlockState(pos, state.with(ORE_TYPE, random.nextInt(ORES.size())));
     }
 }
--- src/main/java/io/github/theriverelder/randomore/registry/Blocks.java
+++ src/main/java/io/github/theriverelder/randomore/registry/Blocks.java
@@ -5,5 +5,5 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Material;
 
 public class Blocks {
-    public static final RandomOreBlock RANDOM_ORE = new RandomOreBlock(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f, 3.0f));
+    public static final RandomOreBlock RANDOM_ORE = new RandomOreBlock(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f, 3.0f).ticksRandomly());
 }
